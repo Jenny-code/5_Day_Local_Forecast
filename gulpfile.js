a2e4ce9723788ba382bd341f3f6a4ba1e8266ed0
Jennifer Unger <jennifer.m.unger@outlook.com>
2020-05-20 20:18:05 -0500

    Tidying up and finalizing with gulpfiles

--- gulpfile.js
+++ gulpfile.js
@@ -5,18 +5,18 @@ const babel = require('gulp-babel');
 const uglify = require('gulp-uglify');
 
 function transpileAndCompress() {
-  return gulp.src('source/*.js')
+  return gulp.src('./*.js')
     .pipe(babel({
         presets: ['@babel/env']
     }))
     .pipe(uglify())
-    .pipe(gulp.dest('dist/js'))
+    .pipe(gulp.dest('./dist'))
 }
 
 function minifyCSS() {
-  return gulp.src('source/*.css')
+  return gulp.src('./*.css')
     .pipe(cleanCSS())
-    .pipe(gulp.dest('dist/css'));
+    .pipe(gulp.dest('./dist'));
 }
 
 function sync() {
@@ -29,12 +29,12 @@ function sync() {
 
 function copyHTML() {
   return gulp.src('index.html')
-    .pipe(gulp.dest('dist'));
+    .pipe(gulp.dest('./dist'));
 }
 
 exports.default = function() {
-  gulp.watch('source/*.css', minifyCSS);
-  gulp.watch('source/*.js', transpileAndCompress);
+  gulp.watch('./*.css', minifyCSS);
+  gulp.watch('./*.js', transpileAndCompress);
   gulp.watch('index.html', copyHTML);
   minifyCSS();
   transpileAndCompress();
